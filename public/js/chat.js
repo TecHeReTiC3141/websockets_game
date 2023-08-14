dde8c2e55c9acfa252ce9cb356d4441570ae7922
TecHeReTiC <talex73@mail.ru>
2023-08-14 15:28:40 +0500

    Basic chat has been implemented on frontend

---
+++ public/js/chat.js
@@ -0,0 +1,15 @@
+const chat = $('.chat');
+const messageInput = $('#message-input')
+$('.toggle-chat').on('click', function() {
+    $('i', this).toggleClass('fa-caret-up')
+        .toggleClass('fa-caret-down');
+
+    $('.chat-content').toggleClass('open')
+})
+
+$(document).on('keydown', function(ev) {
+    if (ev.code === 'Enter' && messageInput.is(':focus') && messageInput.val()) {
+        console.log(messageInput.val());
+        messageInput.val('');
+    }
+})
